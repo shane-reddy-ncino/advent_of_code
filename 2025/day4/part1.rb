92bf7a883db500c653e937b3dd2927668ddd402b
Shane Reddy <shane.reddy@ncino.com>
2025-12-04 15:51:59 +0000

    day4

---
+++ 2025/day4/part1.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+input = []
+File.readlines('input.txt', chomp: true).each do |line|
+    input << line.chars
+end
+
+def movable_roll?(input, r, c, movable_quota = 4)
+    roll_count = 0
+    # Check row
+    roll_count += 1 if (input&.dig(r, c-1) == '@') && (c-1 >= 0)
+    roll_count += 1 if (input&.dig(r, c+1) == '@')
+
+    # Check column
+    roll_count += 1 if (input&.dig(r-1, c) == '@') && (r-1 >= 0)
+    roll_count += 1 if input&.dig(r+1, c) == '@'
+
+    # Check across
+    roll_count += 1 if input&.dig(r+1, c+1) == '@'
+    roll_count += 1 if (input&.dig(r-1, c-1) == '@') && (r-1 >= 0) && (c-1 >= 0)
+    roll_count += 1 if (input&.dig(r+1, c-1) == '@') && (c-1 >= 0)
+    roll_count += 1 if (input&.dig(r-1, c+1) == '@') && (r-1 >= 0)
+
+    return roll_count < 4
+end
+
+previous_sum = nil
+total_sum = []
+
+new_input = input
+
+while previous_sum != total_sum.sum do
+    count = 0
+    previous_sum = total_sum.sum
+
+    input.each_with_index do |r, ri|
+        r.each_with_index do |c, ci|
+            if movable_roll?(input, ri, ci) && input&.dig(ri, ci) == '@'
+                count += 1
+                new_input[ri][ci] = 'x'
+            end
+        end
+    end
+
+    total_sum << count
+    input = new_input
+end
+
+puts total_sum.sum
